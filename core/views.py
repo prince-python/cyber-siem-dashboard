284d9c8590d9623d92fbb304867a224ea294147d
prince-python <choudharyprince140@gmail.com>
2025-08-02 14:42:42 +0100

    landing page

--- core/views.py
+++ core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Welcome to the SIEM Dashboard</h1>")  
+    return render(request, 'index.html')  # Render the home template
